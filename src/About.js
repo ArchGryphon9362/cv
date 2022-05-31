e1e837be49d5993369897bd1d3f19dbd1d5a8f81
Lex Nastin <lexnastin@gmail.com@>
2022-05-31 10:29:09 +0100

    Begin Work On Website + Routing

---
+++ src/About.js
@@ -0,0 +1,7 @@
+export const About = () => {
+    return (
+        <div id='about'>
+            <p>about page</p>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { About } from './About';
+import { Projects } from './Projects';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>
+    <Routes>
+      <Route path='/' element={<About />} />
+      <Route path='/projects' element={<Projects />} />
+    </Routes>
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
